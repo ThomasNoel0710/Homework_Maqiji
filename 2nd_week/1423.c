b531b3adb0e7701f00c6f1b0a309dc292793112b
Noel <147444220+ThomasNoel0710@users.noreply.github.com>
2025-05-25 22:39:20 +0800

    Update 1423.c

--- 2nd_week/1423.c
+++ 2nd_week/1423.c
@@ -10,20 +10,20 @@ int maxScore(int* cardPoints, int cardPointsSize, int k) {
     int remain = len - k; // 记录剩余数组长度
     int sum = 0; // 记录数组元素之和
 
-    for(int i = 0; i < len; i++) {
+    for (int i = 0; i < len; i++) {
         sum += cardPoints[i];
     }
 
-    for(int i = 0; i < remain; i++) {
+    for (int i = 0; i < remain; i++) {
         // 计算当前分数
         tem += cardPoints[i];
     }
     res = tem;
-    for(int i = remain; i < len; i++) {
+    for (int i = remain; i < len; i++) {
         // 使用滑动窗口更新最大分数
         tem -= cardPoints[i - remain];
         tem += cardPoints[i];
-        res = Min(res,tem); // 更新能够得到的最小的子数组
+        res = Min(res, tem); // 更新能够得到的最小的子数组
     }
 
     res = sum - res;
